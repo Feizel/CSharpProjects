bf31edfcba0417f854e0dbd302153e8ee66888a5
Feizel <feizel.maduna@gmail.com>
2024-01-14 16:51:24 +0200

    A project for practicing If/else statements

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -6,6 +6,14 @@ int roll3 = dice.Next(50, 101);
 
 Console.WriteLine($"First Roll: {roll1}");
 Console.WriteLine($"Second Roll: {roll2}");
-Console.WriteLine($"Third Roll: {roll3}");
+Console.WriteLine($"Third Roll: {roll3} \n\n");
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
 
 
--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d16ca38dd69afb158c0de460b1e7000d55a4c59d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13a83599ce21f4203aad74e3ae42661b097aeeb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
