dde31a3c056cb8991645b5fcf585a71234db64f3
Feizel <feizel.maduna@gmail.com>
2024-01-14 20:47:30 +0200

    improved

--- LogicOperands/Program.cs
+++ LogicOperands/Program.cs
@@ -21,7 +21,9 @@ if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
         System.Console.WriteLine("You rolled triples! +6 bonus to total!\n");
         total += 6;
         System.Console.WriteLine($"Total is now: {total}");
-    } else {
+    }
+    else
+    {
         Console.WriteLine("You rolled doubles! +2 bonus to total!");
         total += 2;
         System.Console.WriteLine($"Total is now: {total}");
@@ -31,13 +33,17 @@ if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 if (total >= 16)
 {
     System.Console.WriteLine("Congratulations! You win a new car");
-} else if (total >= 10)
+}
+else if (total >= 10)
 {
     System.Console.WriteLine("Congratulations! You win a laptop");
-} else if (total == 7)
+}
+else if (total == 7)
 {
     System.Console.WriteLine("Congratulations! You win a trip to Seychelles");
-} else {
-  System.Console.WriteLine("You win a kitten");  
+}
+else
+{
+    System.Console.WriteLine("You win a kitten");
 }
 
