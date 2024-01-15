59e5db98d114b28b66473256e46b869ac204478c
Feizel <feizel.maduna@gmail.com>
2024-01-15 06:49:22 +0200

    changes

--- FraudDetector/Program.cs
+++ FraudDetector/Program.cs
@@ -18,12 +18,35 @@
 // Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
 // Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
 
-string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+// string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
 
-foreach (string orderID in orderIDs)
+// foreach (string orderID in orderIDs)
+// {
+//     if (orderID.StartsWith("B"))
+//     {
+//         Console.WriteLine(orderID);
+//     }
+// }
+
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
 {
-    if (orderID.StartsWith("B"))
-    {
-        Console.WriteLine(orderID);
-    }
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
 }
