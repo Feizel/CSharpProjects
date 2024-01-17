9f3f264df24ae02c27d6203694df245b8f14ee85
Feizel <feizel.maduna@gmail.com>
2024-01-17 08:14:55 +0200

    Final working version & improvements

--- ContosoPetsApp/obj/Debug/net8.0/ContosoPetsApp.AssemblyInfo.cs
+++ ContosoPetsApp/obj/Debug/net8.0/ContosoPetsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPetsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78947afcd4cda7d7d618e6887999732959d2ff44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a5abe740a2c5b36e094d35a6404534b3aef0afe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPetsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPetsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
