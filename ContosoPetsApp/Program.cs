fd16369903f16d6873ab9fff822462ab5809252c
Feizel <feizel.maduna@gmail.com>
2024-01-17 05:36:26 +0200

    Rules and Specifications

--- ContosoPetsApp/Program.cs
+++ ContosoPetsApp/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+An application that helps you manage information about the dogs and cats you're caring fo
+
+The application will:
+
+Add predefined sample data to the pets array.
+Iterate a "menu options and user selection" code block to establish the outer loop of your application.
+Implement code branches corresponding to the user's menu selections.
+Display all the information contained in the array used to store pet data (based on user's menu selection).
+Iterate an "add new animal information" code block that enables the user to add one or more new animals to the pets array (based on user's menu selection).
+
+The specifications for your application are:
+Create a C# console application.
+Store application data in a multidimensional string array named ourAnimals.
+
+The ourAnimals array includes the following "pet characteristics" for each animal:
+Pet ID #.
+Pet species (cat or dog).
+Pet age (years).
+A description of the pet's physical condition/characteristics.
+A description of the pet's personality.
+The pet's nickname.
+Implement a sample dataset that represents dogs and cats currently in your care.
+Display menu options to access the main features of the application.
+
+The main features enable the following tasks:
+List the pet information for all animals in the ourAnimals array.
+
+Add new animals to the ourAnimals array. The following conditions apply:
+The pet species (dog or cat) must be entered when a new animal is added to the ourAnimals array.
+A pet ID must be programmatically generated when a new animal is added to the ourAnimals array.
+Some physical characteristics for a pet may be unknown until a veterinarian's examination. For example: age, breed, and neutered/spayed status.
+An animal's nickname and personality may be unknown when a pet first arrives.
+Ensure animal ages and physical descriptions are complete. This may be required after a veterinarian's examination.
+Ensure animal nicknames and personality descriptions are complete (this action can occur after the team gets to know a pet).
+Edit an animal’s age (if a pet's birth date is known and the pet has a birthday while in our care).
+Edit an animal’s personality description (a pet may behave differently after spending more time in our care).
+Display all cats that meet user specified physical characteristics.
+Display all dogs that meet user specified physical characteristics.
+An initial version of the application has already been completed. The Starter code project for this Guided project module includes a Program.cs file that provides the following code features:
+The code declares variables used to collect and process pet data and menu item selections.
+The code declares the ourAnimals array.
+The code uses a for loop around an if-elseif-else construct to populate the ourAnimals array with a sample dataset.
+The code displays the following main menu options for user selection:
+List all of our current pet information.
+Assign values to the ourAnimals array fields.
+Ensure animal ages and physical descriptions are complete.
+Ensure animal nicknames and personality descriptions are complete.
+Edit an animal’s age.
+Edit an animal’s personality description.
+Display all cats with a specified characteristic.
+Display all dogs with a specified characteristic.
+Enter menu item selection or type "Exit" to exit the program
+The code reads the user's menu item selection and displays a message echoing their selection.
+*/
+
