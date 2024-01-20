e3253c564f667e9976966327a763c691caef468c
Feizel <feizel.maduna@gmail.com>
2024-01-20 07:45:47 +0200

    TryParse method practice

--- TestProject2/Program.cs
+++ TestProject2/Program.cs
@@ -11,3 +11,24 @@ Console.WriteLine("Floating point types:");
 Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
 Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
 Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0;
+string message = "";
+
+foreach (var value in values)
+{
+    decimal number;
+    if (decimal.TryParse(value, out number))
+    {
+        total += number;
+    }
+    else
+    {
+        message += value;
+    }
+}
+
+System.Console.WriteLine($"Total: {total}");
+System.Console.WriteLine($"Message: {message}");
