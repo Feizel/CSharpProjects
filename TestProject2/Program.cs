496934d1d9c70868bfac4fe52d17cec71e2ef8f6
Feizel <feizel.maduna@gmail.com>
2024-01-20 07:46:28 +0200

    minor

--- TestProject2/Program.cs
+++ TestProject2/Program.cs
@@ -10,7 +10,9 @@ Console.WriteLine("");
 Console.WriteLine("Floating point types:");
 Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
 Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)\n\n");
+
+
 
 string[] values = { "12.3", "45", "ABC", "11", "DEF" };
 
