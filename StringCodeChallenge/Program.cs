1778d06a7f29acea67c129c7857cd17f633cb2f0
Feizel <feizel.maduna@gmail.com>
2024-01-21 13:20:38 +0200

    First commit

---
+++ StringCodeChallenge/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+To the sales and marketing company's newest investment products, you send thousands of personalized letters to the company's existing clients. 
+Your job is to write C# code to merge personalized information about the customer. The letter contains information like their existing portfolio and 
+compares their current returns to projected returns if they were to invest in using the new products.
+*/
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+// Your logic here
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+// Your logic here
+
+Console.WriteLine(comparisonMessage);
