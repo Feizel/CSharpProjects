13f265583746cc836c9a3629948f63a51e1a6739
Feizel <feizel.maduna@gmail.com>
2024-01-21 12:22:43 +0200

    CodeChallenge solution

--- CodeChallenge/Program.cs
+++ CodeChallenge/Program.cs
@@ -1,14 +1,38 @@
-﻿int value1 = 12;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
+﻿// int value1 = 12;
+// decimal value2 = 6.2m;
+// float value3 = 4.3f;
 
-// The Convert class is best for converting the fractional decimal numbers into whole integer numbers
-// Convert.ToInt32() rounds up the way you would expect.
-int result1 = Convert.ToInt32((decimal)value1 / value2);
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+// // The Convert class is best for converting the fractional decimal numbers into whole integer numbers
+// // Convert.ToInt32() rounds up the way you would expect.
+// int result1 = Convert.ToInt32((decimal)value1 / value2);
+// Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-decimal result2 = value2 / (decimal)value3;
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+// decimal result2 = value2 / (decimal)value3;
+// Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-float result3 = value3 / value1;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+// float result3 = value3 / value1;
+// Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+/*
+Write code to reverse each word in a message
+*/
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+// Step 1
+string[] message = pangram.Split(' ');
+
+//Step 2
+string[] newMessage = new string[message.Length];
+
+// Step 3
+for (int i = 0; i < message.Length; i++)
+{
+    char[] letters = message[i].ToCharArray();
+    Array.Reverse(letters);
+    newMessage[i] = new string(letters);
+}
+
+//Step 4
+string result = String.Join(" ", newMessage);
+Console.WriteLine(result);
--- CodeChallenge/obj/Debug/net8.0/CodeChallenge.AssemblyInfo.cs
+++ CodeChallenge/obj/Debug/net8.0/CodeChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ef5fca9801258d243865c9b7d289e455b4bd780")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+224b7bd520f7e99ca128592677eb311cbee91b4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
