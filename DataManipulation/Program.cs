20fa1a4259491f798673df7a3be91dbf8c42c36c
Feizel <feizel.maduna@gmail.com>
2024-01-21 10:52:43 +0200

    First commit

---
+++ DataManipulation/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+Suppose you are a software developer on a team assigned to work with a logistics company. 
+They have many needs for gathering and organizing their business data. 
+Projects vary from tracking and reporting inventory, 
+enabling specific data to be passed to and from business partners, and identifying possible fraudulent orders. 
+Each project is different, but all require the processing of data. 
+In these projects the applications will be performing data operations such as to add, 
+delete, sort, combine, calculate, validate and format output.
+*/
