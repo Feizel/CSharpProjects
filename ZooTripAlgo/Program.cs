e70336d91da2ddb5d782b1f0f37e6e678fc47780
Feizel <feizel.maduna@gmail.com>
2024-01-22 11:01:24 +0200

    First data

--- ZooTripAlgo/Program.cs
+++ ZooTripAlgo/Program.cs
@@ -6,3 +6,8 @@ string[] pettingZoo =
     "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
+
+// RandomizeAnimals();
+// string[,] group = AssignGroup();
+Console.WriteLine("School A");
+// PrintGroup(group);
