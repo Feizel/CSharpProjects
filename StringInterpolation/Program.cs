288b7939d749f13f40d70cd217dded45fe38a57c
Feizel <feizel.maduna@gmail.com>
2024-02-01 11:06:07 +0200

    Playing with strings

--- StringInterpolation/Program.cs
+++ StringInterpolation/Program.cs
@@ -1,27 +1,51 @@
-﻿/*
-You need to create the code to print a receipt for the customer purchasing shares of an investment product. 
-The shares are purchased automatically at the end of the year based on a series of payroll deductions, 
-so the number of shares purchased usually contains a decimal amount. 
-To print the receipt, you would likely need to combine data of different types, including fractional values, 
-currency, and percentages in precise ways.
-*/
+﻿using System;
 
-int invoiceNumber = 1201;
-decimal productShares = 25.4568m;
-decimal subtotal = 2750.00m;
-decimal taxPercentage = .15825m;
-decimal total = 3185.19m;
+namespace StoryTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare the variables
+            string beginning = "Once upon a time,";
+            string middle = "The kid climbed a tree";
+            string end = "Everyone lived happily ever after.";
 
-Console.WriteLine($"Invoice Number: {invoiceNumber}");
-Console.WriteLine($"   Shares: {productShares:N3} Product");
-Console.WriteLine($"     Sub Total: {subtotal:C}");
-Console.WriteLine($"           Tax: {taxPercentage:P2}");
-Console.WriteLine($"     Total Billed: {total:C}");
+            // Interpolate the string and the variables
+            string story = $"{beginning} there lived a family with a kid. {middle} and got the fruits for his family. {end}";
 
-string paymentId = "769C";
-string payeeName = "Mr. Stephen Ortega";
+            // Print the story to the console 
+            Console.WriteLine(story);
 
-var formattedLine = paymentId.PadRight(6);
-formattedLine += payeeName.PadRight(24);
 
-Console.WriteLine(formattedLine);
+            /*
+            You need to create the code to print a receipt for the customer purchasing shares of an investment product. 
+            The shares are purchased automatically at the end of the year based on a series of payroll deductions, 
+            so the number of shares purchased usually contains a decimal amount. 
+            To print the receipt, you would likely need to combine data of different types, including fractional values, 
+            currency, and percentages in precise ways.
+            */
+
+            int invoiceNumber = 1201;
+            decimal productShares = 25.4568m;
+            decimal subtotal = 2750.00m;
+            decimal taxPercentage = .15825m;
+            decimal total = 3185.19m;
+
+            Console.WriteLine($"Invoice Number: {invoiceNumber}");
+            Console.WriteLine($"   Shares: {productShares:N3} Product");
+            Console.WriteLine($"     Sub Total: {subtotal:C}");
+            Console.WriteLine($"           Tax: {taxPercentage:P2}");
+            Console.WriteLine($"     Total Billed: {total:C}");
+
+            string paymentId = "769C";
+            string payeeName = "Mr. Stephen Ortega";
+
+            var formattedLine = paymentId.PadRight(6);
+            formattedLine += payeeName.PadRight(24);
+
+            Console.WriteLine(formattedLine);
+
+        }
+    }
+}
